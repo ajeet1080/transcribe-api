ce01ebdd54c328654a0eae15444128b6a665cb1e
Ajeet <08.ajeet@gmail.com>
2024-01-27 17:21:04 +0800

    Changed game for testing

--- app.py
+++ app.py
@@ -21,71 +21,43 @@ openai.api_key = "fefc20d1c3ee4046b446c239f96e4fc4"
 @app.route('/solve-mystery', methods=['POST'])
 def solve_mystery():
     user_input = request.json['text']
-    initial_context = """You are a database of evidence that will be used to solve the mystery of the missing merlion. Detectives will ask you questions to retrieve the evidence. Use the following guidelines when giving answers:
+    initial_context = """You are a database of evidence that will be used to solve the mystery of the missing Boss , Head of Dept of Office of Insights and Analytics. Participants will ask you questions to retrieve the information. Use the following guidelines when giving answers:
 
-Answer questions in less than 50 words.
-If asked for the clues or evidence, reply with a random Singapore fun fact.
-If the answer cannot be found, create a random story.
-If the question is irrelevant, reply with a random Singapore fun fact.
+  - Answer questions in less than 50 words in an indirect manner
+  - If asked for the clues or evidence, reply with a poem on the weather
+  - If the answer is unavailable, reply with a random song title
+  - If the question is irrelevant, reply with a fun fact in a cryptic manner
+ 
 
-Evidence
+Clues
 
-Merlion Sightings: The merlion was last seen by credible sources at 1:30am on 2 Jan 2024.
+Mark was last seen online 1 hour ago
 
-Site investigations:
- -Only nuts and bolts were left scattered around the scene.
- -The floor was undamaged and there are no signs of the Merlion being forcefully removed.
- -Skidding marks were seen towards the direction of the river.
+Kwok Ming arranged the meeting 2 days ago and Mark accepted the invite
 
-Surveillance footage:
- -A medium-sized box truck was seen entering the Merlion park on 2 Jan 1:58am. The truck left the park at 3:12am.
- -The license plate on the truck is GBF 2546 J.
- -The truck is registered to “Lion Construction Company”.
+Mark does not have other meetings scheduled before the meeting with Kwok Meng
 
-Lion Construction Company
- -The company is headquartered in Paya Lebar industrial estate and is a licensed contractor for the merlion maintenance.
- -The company was requested by STB to conduct an unscheduled maintenance on 2 Jan, with instructions to leave the equipment on-site after maintenance.
- -Bob was rostered to conduct the merlion maintenance on 2 Jan early morning.
- -Preliminary investigations show Bob to be an animal lover with the lion as his favourite animal.
+Laurence reported that he last saw Mark at the C3 meeting in the morning
 
-Singapore Tourism Board (STB)
- -STB oversees scheduling the merlion maintenance.
- -The maintenance schedule is publicly available on the STB website.
- -John is the manager in charge of scheduling maintenance for tourist attractions.
- -John instructed Lion Construction Company to leave the equipment at the site after conducting maintenance.
+Kwok Meng wants to discuss updates on Healix
 
-John
- -John has been working at STB for 5 years.
- -John is a patron of the arts and is active on social media promoting up-and-coming artist.
- -Just before the merlion disappearance, there were unusual telephone logs to an unknown number.
+Michelle and Jun Jie have a scheduled meeting with Mark after the meeting with Kwok Meng to discuss regarding new Data Science project. They are currently in the meeting room waiting for Mark.
 
-Interviews from Members of the Public
- -Prior to the disappearance, visitors interviewed mentioned that the merlion did not seem securely fastened to the base and would wobble when winds were strong.
+Mark returned from an overseas trip last week
 
-Maritime Logs on 2 Jan
- -There were many boats moored along the Singapore River that night.
- -The only active boat during the night was Bumboat “X19545B”.
- -3:20am: Bumboat “X19545B” was in the vicinity of the merlion.
- -4:10am: Bumboat “X19545B” was in the vicinity of the Keppel Bay.
+After meeting Kwok Meng, Mark has a meeting with Ajeet on GenAI
 
-Barge “X19545B”
- -20m long vessel belonging to “Boating Forever Club”
- -Rented by Singapore Art Forever Society from 1 Jan to 3 Jan.
- -The rental company noted that the boat was returned with 10m of heavy-duty sailing rope left on the deck.
+Mark told Elian that he will be coming to the office
 
-Singapore Art Forever Society
- -The society was founded in 1995 by 48-year-old Sally
- -The society owns Warehouse 21 along Keppel Bay
- -The society is known to attract struggling avant-garde sculptors.
+Mark usually travels to work by car and it takes him 1 hour
 
-Warehouse 21
- -Preliminary visits to the warehouse show that it is locked with the windows blacked out. Investigators were unable to view the contents inside.
- -Interviews from neighbouring warehouses state that heavy machine noises have been heard recently.
+Ajeet just arrived in office, coming back from the Microsoft workshop
 
-Sally
- -Sally is a pet lover with 5 cats, 3 dogs and a goldfish.
- -Past posts on IG shows her frustration with the governments lack of funding for the local arts scene.
- -Recently posted on IG that she will unveiling a surprise sculpture on Chinese New Year eve. The post was liked by John."""
+Angela has been helping Mark in managing his calendar. People are trying to reach her to find out Mark's whereabouts.
+
+Only Poh Lai, Esther and Steven and Kwok Meng were in the office in the morning, everyone else was at the Microsoft workshop.
+
+The office is quite today."""
 
     try:
         response = openai.ChatCompletion.create(
@@ -111,15 +83,7 @@ def get_semantic_similarity(received, actual):
 @app.route('/similarity_score', methods=['POST'])
 def get_similarity_response():
     user_message = request.json.get('text')
-    ideal_answer = """ Who: Sally, a 48 year-old avant-garde sculptor
-
-When: 3:20am
-
-Where: Merlion Park
-
-How: Arriving via barge, Sally dismantled the Merlion using tools left behind by the maintenance company – Lion Construction Company – and hauled the Merlion onto the barge using ropes. She then sailed to Warehouse 21 along Keppel Bay to re-work the Merlion into a sculpture to be unveiled on Chinese New Year eve.
-
-Why: Struggling to gain recognition for her works, she decided to use the iconic Merlion status as the base for her latest work as a form of protest and attract attention."""
+    ideal_answer = """Mark is in his car driving to work.\nHe is driving to office from home.\nHe will be arriving in 5 minutes to meet Kwok Meng."""
     response = openai.ChatCompletion.create(
         # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
         engine="432",
@@ -132,18 +96,7 @@ Why: Struggling to gain recognition for her works, she decided to use the iconic
 
 
 
-IDEAL ANSWER:
-
-    Who: Sally, a 48 year-old avant-garde sculptor
-
-    When: 3:20am
-
-    Where: Merlion Park
-
-    How: Arriving via barge, Sally dismantled the Merlion using tools left behind by the maintenance company , Lion Construction Company , and hauled the Merlion onto the barge using ropes. She then sailed to Warehouse 21 along Keppel Bay to re-work the Merlion into a sculpture to be unveiled on Chinese New Year eve.
-
-    Why: Struggling to gain recognition for her works, she decided to use the iconic Merlion status as the base for her latest work as a form of protest and attract attention.
-
+IDEAL ANSWER: \nMark is in his car driving to work.\nHe is driving to office from home.\nHe will be arriving in 5 minutes to meet Kwok Meng.
                 """},
             {"role": "user", "content": user_message}
         ] , temperature=0.01,top_p=1 
