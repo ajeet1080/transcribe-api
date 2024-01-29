750cf7220237267a596d38842a6cb748420731ea
Ajeet <08.ajeet@gmail.com>
2024-01-29 18:43:42 +0800

    Update api

--- app.py
+++ app.py
@@ -21,47 +21,70 @@ openai.api_key = "fefc20d1c3ee4046b446c239f96e4fc4"
 @app.route('/solve-mystery', methods=['POST'])
 def solve_mystery():
     user_input = request.json['text']
-    initial_context = """You are a database of evidence that will be used to solve the mystery of the missing Boss , Head of Dept of Office of Insights and Analytics. Participants will ask you questions to retrieve the information. Use the following guidelines when giving answers:
+    initial_context = """You are a database of evidence that will be used to solve the mystery of the missing merlion. Detectives will ask you questions to retrieve the evidence. Use the following guidelines when giving answers:
 
-  - Answer questions in less than 50 words in an indirect manner
-  - If asked for the clues or evidence, reply with a poem on the weather
-  - If the answer is unavailable, reply with a random song title
-  - If the question is irrelevant, reply with a fun fact in a cryptic manner
- 
+    - Answer questions in less than 50 words in an indirect manner.
+    - If asked for the clues or evidence, reply with a random Singapore fun fact.
+    - If asked for the answer to the mystery (e.g. who is involved, how it happened, motivation), reply that you do not know
+    - If the information is not available, make up an answer.
 
-Clues
+Evidence
 
-Mark was last seen online 1 hour ago
+Merlion Sightings:
+  -The merlion was last seen by credible sources at 1:30am on 2 Jan 2024.
+  -When the police arrived at 6:45am, the Merlion was missing with a large crowd gathered taking photos.
 
-Kwok Ming arranged the meeting 2 days ago and Mark accepted the invite
+Site investigations:
+  -Only nuts and bolts were left scattered around the scene.
+  -Damage to the floor was minimal and there were no signs of the Merlion being forcefully removed.
+  -Skidding marks were seen towards the direction of the river.
 
-Mark does not have other meetings scheduled before the meeting with Kwok Meng
+Surveillance footage:
+  -A medium-sized box truck was seen entering the Merlion park on 2 Jan 1:58am. The truck left the park at 3:12am.
+  -The license plate on the truck is GBF 2546 J.
+  -The truck is registered to “Lion Construction Company”.
 
-Laurence reported that he last saw Mark at the C3 meeting in the morning
+Lion Construction Company
+  -The company is headquartered in Paya Lebar industrial estate and is a licensed contractor for the merlion maintenance.
+  -The company has been contracted by STB for the last 10 years.
+  -The company was requested by STB to conduct an unscheduled maintenance on 2 Jan, with instructions to leave the equipment on-site after maintenance.
+  -Bob was rostered to conduct the merlion maintenance on 2 Jan early morning.
+  -Bob logged that the maintenance was completed at 3am.
+  -Preliminary investigations show Bob to be an animal lover with the lion as his favourite animal.
 
-Kwok Meng wants to discuss updates on Healix
+Singapore Tourism Board (STB)
+  -STB oversees scheduling the merlion maintenance.
+  -The maintenance schedule is publicly available on the STB website.
+  -John is the manager in charge of scheduling maintenance for tourist attractions.
+  -John instructed Lion Construction Company to leave the equipment at the site after conducting maintenance.
 
-Michelle and Jun Jie have a scheduled meeting with Mark after the meeting with Kwok Meng to discuss regarding new Data Science project. They are currently in the meeting room waiting for Mark.
+John
+  -John has been working at STB for 5 years.
+  -John is a patron of the arts and is active on social media promoting up-and-coming artist.
+  -Just before the merlion disappearance, there were unusual telephone logs to an unknown number.
 
-Mark returned from an overseas trip last week
+Interviews from Members of the Public
+  -Prior to the disappearance, visitors interviewed mentioned that the merlion did not seem securely fastened to the base and would wobble when winds were strong.
 
-After meeting Kwok Meng, Mark has a meeting with Ajeet on GenAI
+Maritime Logs on 2 Jan
+  -There were many boats moored along the Singapore River that night.
+  -The only active boat during the night was Bumboat “X19545B”.
+  -3:20am: Bumboat “X19545B” was in the vicinity of the merlion.
+  -4:10am: Bumboat “X19545B” was in the vicinity of the Keppel Bay.
 
-Mark told Elian that he will be coming to the office
+Barge “X19545B”
+  -20m long vessel belonging to “Boating Forever Club”
+  -Rented by Singapore Art Forever Society from 1 Jan to 3 Jan.
+  -The rental company noted that the boat was returned with 10m of heavy-duty sailing rope left on the deck.
 
-Mark usually travels to work by car and it takes him 1 hour
-
-Ajeet just arrived in office, coming back from the Microsoft workshop
-
-Angela has been helping Mark in managing his calendar. People are trying to reach her to find out Mark's whereabouts.
-
-Only Poh Lai, Esther and Steven and Kwok Meng were in the office in the morning, everyone else was at the Microsoft workshop.
-
-The office is quite today."""
+Singapore Art Forever Society
+  -The society was founded in 1995 by 48-year-old Sally
+  -The society owns Warehouse 21 along Keppel Bay
+  -The society is known to attract struggling avant-garde sculptors."""
 
     try:
         response = openai.ChatCompletion.create(
-            engine="432",
+            engine="4Base",
             messages=[
                 {"role": "system", "content": initial_context},
                 {"role": "assistant", "content": user_input}
@@ -83,20 +106,39 @@ def get_semantic_similarity(received, actual):
 @app.route('/similarity_score', methods=['POST'])
 def get_similarity_response():
     user_message = request.json.get('text')
-    ideal_answer = """Mark is in his car driving to work.\nHe is driving to office from home.\nHe will be arriving in 5 minutes to meet Kwok Meng."""
+    ideal_answer = """ Who: Sally, a 48 year-old avant-garde sculptor
+
+When: 3:20am
+
+Where: Merlion Park
+
+How: Arriving via barge, Sally dismantled the Merlion using tools left behind by the maintenance company – Lion Construction Company – and hauled the Merlion onto the barge using ropes. She then sailed to Warehouse 21 along Keppel Bay to re-work the Merlion into a sculpture to be unveiled on Chinese New Year eve.
+
+Why: Struggling to gain recognition for her works, she decided to use the iconic Merlion status as the base for her latest work as a form of protest and attract attention."""
     response = openai.ChatCompletion.create(
         # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
-        engine="432",
+        engine="4Base",
         messages=[
             {"role": "assistant",
                 "content": """You are scientific bot which is programmed to compare  2 set of texts.\n\n User has been asked to solve a mystery of the missing merlion. You will be provided with a list of sentences  which will represent response by user to a detective game which will indicate who stole Merlion statue with other details like when statue was stle, where did it happen , how it happen and what was the motive behind the theft. You will be also provided with ideal answer to the detective game. You need to find the similarity between the ideal answer and the provided sentences and  you need to provide reasoning eg: Becasue you mentioned the correct person but the time was wrong. You missed to mention the reason for the theft. \n\n You should not show score in response , only provide reasoning. Please follow below rules while giving response:\n\n
     - Provide reason in less than 60 words.
-    - Provide reason including missing details on high level. eg. because you mentioned the correct reason for late.You missed to mention why is was late.
+    - Provide reason including missing details on high level. eg. because you mentioned the correct person but the time was wrong. You missed to mention the reason for the theft.
     - Always provide the reason in the following format. Do not provide answers in the reason.:  because <reason>.
 
 
 
-IDEAL ANSWER: \nMark is in his car driving to work.\nHe is driving to office from home.\nHe will be arriving in 5 minutes to meet Kwok Meng.
+IDEAL ANSWER:
+
+    Who: Sally, a 48 year-old avant-garde sculptor
+
+    When: 3:20am
+
+    Where: Merlion Park
+
+    How: Arriving via barge, Sally dismantled the Merlion using tools left behind by the maintenance company , Lion Construction Company , and hauled the Merlion onto the barge using ropes. She then sailed to Warehouse 21 along Keppel Bay to re-work the Merlion into a sculpture to be unveiled on Chinese New Year eve.
+
+    Why: Struggling to gain recognition for her works, she decided to use the iconic Merlion status as the base for her latest work as a form of protest and attract attention.
+
                 """},
             {"role": "user", "content": user_message}
         ] , temperature=0.01,top_p=1 
